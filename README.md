35393e04db6b2dd8f4c8a6fe2c51a0186a5d3fda
Mike Boers <github@mikeboers.com>
2011-04-05 10:41:28 -0400

    Changed status to inactive.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-**TreeSoup** is designed to roughly reflect the BeautifulStoneSoup API, but
+TreeSoup
+=========
+
+**This module is not maintained.** I have moved on to using lxml myself, and so will not maintain this project.
+
+---
+
+`TreeSoup` is designed to roughly reflect the BeautifulStoneSoup API, but
 uses the (standard library) ElementTree to do the parsing. This is mainly
 because BeautifulSoup does not handle unicode very gracefully.
--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@ from distutils.core import setup
 
 setup(
     name='TreeSoup',
-    version='0.1a',
+    version='0.1',
     description='BeautifulSoup-like wrapper around ElementTree.',
     url='http://github.com/mikeboers/TreeSoup',
     py_modules=['treesoup'],
     
     author='Mike Boers',
     author_email='treesoup@mikeboers.com',
-    license='New BSD License',
+    license='BSD-3',
+    
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
 )
